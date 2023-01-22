4baf4aed2c3a7b2862accbc2d64e70e73d3bcb4f
Jafet Zagal <zagalids@gmail.com>
2023-01-21 20:28:26 -0600

    navbar and footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,18 +1,17 @@
 <footer id="footer" class="dark">
 
-    <!-- Copyrights
-    ============================================= -->
+    {{-- Copyrights --}}
     <div id="copyrights">
 
         <div class="container clearfix">
 
             <div class="center">
-                Copyrights &copy; 2020 All Rights Reserved by Canvas Inc.<br>
+                Copyrights &copy; 2023 All Rights Reserved by MSTECS.<br>
                 <div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
             </div>
 
         </div>
 
-    </div><!-- #copyrights end -->
+    </div>{{-- copyrights end --}}
 
 </footer>
